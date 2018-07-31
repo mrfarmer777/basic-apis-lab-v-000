f43992af0f3119a32bffe2c0d5bf7cd1768a7285
Matt Farmer <mrfarmer777@gmail.com>
2018-07-31 02:04:11 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -9,7 +9,7 @@ class RepositoriesController < ApplicationController
     @resp=Faraday.get "https://api.github.com/search/repositories" do |req|
       req.params['q']=qString
       req.params["client_id"]="bdc2081ffef178b404a7"
-      req.params["client_secret"]=""
+      req.params["client_secret"]="1efad0ef39b323c5ecdfa46ab258e849e54e2b7d"
     end
     body=JSON.parse(@resp.body)
     if @resp.success?
