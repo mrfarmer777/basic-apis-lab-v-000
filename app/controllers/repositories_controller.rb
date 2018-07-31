f0222653ddd7d3c703cc97674f3206e6725b09a1
Matt Farmer <mrfarmer777@gmail.com>
2018-07-31 01:52:35 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,6 +5,6 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    @resp=Faraday.get()
+    @resp=Faraday.get("https://api.github.com/search/repositories")
   end
 end
